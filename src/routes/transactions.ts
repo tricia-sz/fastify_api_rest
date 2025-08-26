a344c3bb158353e754f7d31f2ab96214d5f71865
Patricia Souza <patricia.sz.br@gmail.com>
2025-08-26 10:02:03 -0300

    Criando transacoes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,26 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions')
-      .where('amount', 1000)
-      .select('*')
+  app.post('/', async (request, replay) => {
+    const createTransationBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transactions
+    const { title, amount, type } = createTransationBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amout: type === 'credit' ? amount : amount * -1,
+    })
+
+    return replay.status(201).send()
   })
 }
